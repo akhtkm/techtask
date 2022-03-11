f8b26b3cb7a986e42ab16c1c9fb30b98b2af0fd6
akihito.kimura <akihito.kimura.15@gmail.com>
2022-03-11 21:02:22 +0900

    fix parameter

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.vpc.id}"
   tags =   {
-    Name = "${var.environment-igw}"
+    Name = "${var.environment}-igw"
     Environment = "${var.environment}"
   }
 }
